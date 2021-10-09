2282308c9d6b4a602a6f67e3328bb19876250bb7
cbott <cbott6@gmail.com>
2021-10-09 12:25:52 -0700

    Fix robot motion

--- pyworkspace/ax12.py
+++ pyworkspace/ax12.py
@@ -1,10 +1,13 @@
 # micropython for Raspberyy Pi Pico
-import binascii
+from collections import namedtuple
 from machine import UART, Pin
+from math import atan2, cos, pi, radians, sin
 import struct
 import utime
-import random
 
+
+class AX12ResponseError(Exception):
+    pass  # TODO: Subclass further?
 
 class AX12:
     """ Hardware class for Dynamixel AX-12 servo line
@@ -61,21 +64,27 @@ class AX12:
 
     # TODO: make a Broadcast() static method, might need to make send_packet more generic
     # TODO: should methods perform bounds checking/constrain inputs?
+    # TODO: Rewrite so that all memory values have getters and setters? Can access as x = goal_position or goal_position = x
 
-    def __init__(self, uart: UART, id: int):
+    def __init__(self, uart: UART, id: int, rxpin: Pin):
         """
         uart: UART object the servo is connected to
         id: Hardware id of servo
+        rxpin: Pin object controlling the connected tri-state buffer. 0=tx, 1=rx
         """
         self.uart = uart
         self.id = id
+        self.rxpin = rxpin
 
     def _send_uart(self, txdata: bytes):
         """ Send supplied data over UART """
         self.uart.write(txdata)
 
     def _send_instruction(self, instruction: int, *params: int):
-        """ Send message to servo in Dynamixel format """
+        """ Send message to servo in Dynamixel format, return list of response parameters """
+        # Set to TX mode in advance of writing to the bus
+        self.rxpin.value(0)  # TODO: don't hard code 0/1?
+
         # Dynamixel-defined Length field
         length = len(params) + 2
 
@@ -88,7 +97,61 @@ class AX12:
         num_bytes = len(message_contents)
 
         message = struct.pack('{}b'.format(num_bytes), *message_contents)
+
+        # Send the instruction
+        # print('[0x{:x}] Sending message {}'.format(self.id, message))
         self._send_uart(message)
+        utime.sleep_us(1)  # Empirically determined delay to let uart finish transmitting I guess?
+
+        # Read the status packet response
+        self.rxpin.value(1)
+        utime.sleep_ms(10)  # Dynamixel response delay is 500us, plus a bit of margin TODO: re-evaluate. 5ms didn't seem to be reliable with multiple servos on the bus
+        rxData = self.uart.read()  # Read in all available data
+        self.rxpin.value(0)
+
+        if not rxData or len(rxData) < 6:
+            print(rxData)
+            raise AX12ResponseError('[0x{:x}] Did not receive response from AX12 device'.format(self.id))
+
+        # Response should be 7 bytes
+        # 0x FF  FF  03  03   00   [XX] [XX] [...]  C6
+        #    HDR HDR ID  LEN  ERR  [P1] [P2] [P...] CHK
+        # For some reason we also seem to read in a \x00 first, throw that out if it exists
+        if rxData.startswith(b'\x00'):
+            rxData = rxData[1:]
+        if not rxData.startswith(bytes(2 * [AX12.MESSAGE_HEADER])):
+            raise AX12ResponseError('[0x{:x}] Recieved data {} does not have expected message header'.format(self.id, rxData))
+
+        # print('Data', rxData)
+        # response = struct.unpack('7B', rxData)
+        r_id = rxData[2]
+        r_len = rxData[3]
+        r_err = rxData[4]
+        r_params = [i for i in rxData[5:-1]]
+        r_checksum = rxData[-1]
+
+        # print('Decoded', r_id, r_len, r_err, r_params, r_checksum)
+        checksum = (~(r_id + r_len + r_err + sum(r_params))) & 0xFF
+
+        if checksum != r_checksum:
+            raise AX12ResponseError('[0x{:x}] Calculated checksum {} does not match packet checksum {}'.format(self.id, checksum, r_checksum))
+
+        # Error codes
+        # 1000000 Instruction Error   In case of sending an undefined instruction or delivering the action instruction without the Reg Write instruction, it is set as 1
+        # 0100000 Overload Error      When the current load cannot be controlled by the set Torque, it is set as 1
+        # 0010000 Checksum Error      When the Checksum of the transmitted Instruction Packet is incorrect, it is set as 1
+        # 0001000 Range Error         When an instruction is out of the range for use, it is set as 1
+        # 0000100 Overheating Error   When internal temperature of DYNAMIXEL is out of the range of operating temperature set in the Control table, it is set as 1
+        # 0000010 Angle Limit Error   When Goal Position is written out of the range from CW Angle Limit to CCW Angle Limit , it is set as 1
+        # 0000001 Input Voltage Error When the applied voltage is out of the range of operating voltage set in the Control table, it is as 1
+        if r_err:
+            raise AX12ResponseError('[0x{:x}] Status packet returned nonzero error byte {:08b}'.format(self.id, r_err))
+
+        num_parameters = r_len - 2  # Dynamixel spec
+        if num_parameters != len(r_params):
+            raise AX12ResponseError('[0x{:x}] Unexpected number of parameters received. Expected {} parameter(s), received [{}]'.format(self.id, num_parameters, r_params))
+
+        return r_params
 
     def write_led_status(self, status: bool):
         """
@@ -132,45 +195,33 @@ class AX12:
         self._send_instruction(AX12.CMD_WRITE, AX12.MEM_ID, id)
         self.id = id
 
-    def read_temperature(self):
+    def write_torque_enable(self, enable: bool):
+        """ Enable or disable servo torque output """
+        self._send_instruction(AX12.CMD_WRITE, AX12.MEM_TORQUE_ENABLE, enable)  # type: ignore
+
+    def read_temperature(self) -> int:
         """ Present Tempearatrue
 
         Return temperature as read from the AX-12
         """
         # Send the read instruction
         data_length = 0x1  # Read 1 byte of data from the specified memory address
-        self._send_instruction(AX12.CMD_READ, AX12.MEM_PRESENT_TEMPERATURE, data_length)
+        r_params = self._send_instruction(AX12.CMD_READ, AX12.MEM_PRESENT_TEMPERATURE, data_length)
+        if len(r_params) != 1:
+            raise ValueError('Received {} response parameters, expected 1'.format(len(r_params)))
+        return r_params[0]
 
-        # TODO: Clearing the input buffer breaks things?
-        # uart0.read(uart0.any())  # Clear the input buffer
+    def read_position(self) -> int:
+        """ Present Position
 
-        # Set the TX pin to floating so it doesn't interfere with RX
-        # TODO: Hard coded pin value
-        txpin = Pin(0, mode=Pin.IN)
+        Return AX-12 position in counts (0-1023)
+        """
+        r_params = self._send_instruction(AX12.CMD_READ, AX12.MEM_PRESENT_POSITION, 0x2)
+        return r_params[0] + (r_params[1] << 8)
 
-        # rxData = bytes()
-        # while self.uart.any() > 0:
-        #     rxData += self.uart.read(1)
-        rxData = self.uart.read()  # Read in all available data
+# TODO: maybe this is class specific, store in Robot
+Pose = namedtuple('Pose', ['theta0', 'theta1', 'theta2', 'theta3'])
 
-        if not rxData or len(rxData) < 7:
-            raise ValueError('Did not receive response from AX12 device')
-
-        # Response should be 7 bytes, discard all the beginning stuff
-        # 0x FF  FF  03  03   00   33  C6
-        #    HDR HDR ID  LEN  ERR  T   CHK
-        response_data = rxData[-7:]
-        r_id, r_len, r_err, r_temperature, r_checksum = struct.unpack('7B', response_data)
-        checksum = (~(r_id + r_len + r_err + r_temperature)) & 0xFF
-
-        # Set the UART back to send mode
-        # TODO: instead of this just set the pin mode back
-        uart0 = UART(0, baudrate=BAUDRATE, tx=txpin, rx=Pin(1))
-
-        if checksum != r_checksum:
-            raise ValueError('Calculated checksum {} does not match packet checksum {}'.format(checksum, r_checksum))
-
-        return r_temperature
 
 class Robot:
     NUM_JOINTS = 4
@@ -180,23 +231,131 @@ class Robot:
         (45, 1000),
         (45, 1000)
     ]
+    # Beam lengths [mm]
+    L1 = 120
+    L2 = 115
+    L3 = 57
 
-    def __init__(self, uart: UART) -> None:
-        self.joints = [AX12(uart, 0x1),
-                       AX12(uart, 0x2),
-                       AX12(uart, 0x3),
-                       AX12(uart, 0x4)]
+    def __init__(self, servo1, servo2, servo3, servo4) -> None:
+        self.joints = [servo1, servo2, servo3, servo4]
 
         for joint_num, joint in enumerate(self.joints):
             joint.write_cw_limit(self.JOINT_LIMITS[joint_num][0])
             joint.write_ccw_limit(self.JOINT_LIMITS[joint_num][1])
             joint.write_speed(100)
 
-    def set_joint(self, joint_num: int, position: int) -> None:
+        self.prev_angles = Pose(pi/2, 2, -pi/2, -pi/2)
+        self.set_joint_angles(self.prev_angles)
+
+    def set_joint_raw(self, joint_num: int, position: int) -> None:
+        """ Set joint position directly as Servo value, after rounding to nearest int """
         # TODO: Add bounds checking or read error flag after write
         # maybe write_position needs to return a bool and we pass that along?
         self.joints[joint_num].write_position(position)
 
+    def set_joint_angles(self, angles: Pose, speed: int = 100) -> bool:
+        """ Set angles in radians """
+        # Determine motion speeds
+        # TODO: should we make a standard "angle list" type to pass around? Probably not but could still hold in a list
+        n = len(angles)  # TODO: should use class constant num joints
+        deltas = [abs(angles[i] - self.prev_angles[i]) for i in range(n)]
+        max_delta = max(deltas)
+        if max_delta > 0:
+            for joint_num in range(n):
+                self.joints[joint_num].write_speed(int(speed * deltas[joint_num] / max_delta))
+        self.prev_angles = angles
+
+        # Convert angle to servo counts
+        # TODO: determine if 0.29 is accurate
+        COUNTS_PER_RADIAN = 197.571654  # 0.29 degrees per count
+        positions = [
+            (angles.theta0 * COUNTS_PER_RADIAN) + 201,
+             822 - (angles.theta1 * COUNTS_PER_RADIAN),  # Or (angles.theta1 * COUNTS_PER_RADIAN) + 201 if we fix servo direction
+             512 - (angles.theta2 * COUNTS_PER_RADIAN),  # Or (angles.theta2 * COUNTS_PER_RADIAN) + 512
+             512 - (angles.theta3 * COUNTS_PER_RADIAN)  # Or (angles.theta3 * COUNTS_PER_RADIAN) + 512
+        ]
+        positions = [int(round(p, 0)) for p in positions]
+
+        print('Setting joint positions of {}'.format(positions))
+
+        for joint in range(self.NUM_JOINTS):
+            limits = self.JOINT_LIMITS[joint]
+            if not limits[0] <= positions[joint] <= limits[1]:
+                return False
+
+        for joint in range(self.NUM_JOINTS):
+            self.set_joint_raw(joint, positions[joint])
+        return True
+
+    @classmethod
+    def inverse_kinematics(cls, x: float, y: float, z:float, end_angle: float = 0, invert_base: bool = False, invert_elbow: bool = False) -> Pose:
+        """ Determine necessary joint angles to reach a position """
+        # Solve for joint 0 angle
+        theta0 = atan2(y, x)
+        if invert_base:
+            # When we want to "reach over backwards" the base points the opposite direction of the desired point
+            theta0 += pi
+        # Constrain base to the valid servo range of -pi/3 to 8pi/6
+        # We may still end up in an invalid range, caller must ensure this is not the case
+        if theta0 < -pi/3:
+            theta0 += 2*pi
+        if theta0 > 8*pi/6:
+            theta0 -= 2*pi
+
+        # Translate end effector coordinates to joint 3 coordinates
+        d_z = cls.L3 * sin(end_angle)
+        d_r = cls.L3 * cos(end_angle)
+        x = x - d_r * cos(theta0)
+        y = y - d_r * sin(theta0)
+        z = z - d_z
+
+        # Solve for joint 2 angle
+        dist_squared = x**2 + y**2 + z**2
+        # solve for cos(theta2) using law of cosines
+        cos_theta2 = (dist_squared - cls.L1**2 - cls.L2**2) / (2 * cls.L1 * cls.L2)
+        if not -1 <= cos_theta2 <= 1:
+            raise ValueError("Target position {} not achievable".format((x, y, z)))
+        sin_theta2 = -(1 - cos_theta2**2)**0.5
+        if invert_elbow:
+            sin_theta2 *= -1
+        theta2 = atan2(sin_theta2, cos_theta2)
+
+        # Solve for joint 1 angle
+        r = (x**2 + y**2)**0.5
+        alpha = atan2(z, r)
+        beta = atan2(cls.L2 * sin(theta2), cls.L2 * cos(theta2) + cls.L1)
+        theta1 = alpha - beta
+
+        # Make corrections for flipped base
+        if invert_base:
+            # Base has been set to face away from the target point, reflect remaining joints about z-r plane
+            theta1 = pi - theta1
+            theta2 *= -1
+
+        # Solve for joint 3 angle
+        # TODO: make this play nice with inverted base
+        theta3 = end_angle - theta1 - theta2
+        # TODO: do this with mod math
+        if theta3 < -pi:
+            theta3 += 2*pi
+        if theta3 > pi:
+            theta3 -= 2*pi
+
+        return Pose(theta0, theta1, theta2, theta3)
+
+    def set_position(self, x: float, y: float, z:float, end_angle: float = 0, invert_base: bool = False, invert_elbow: bool = False, speed: int = 100) -> bool:
+        """ Move the end effector to the specified cartesian coordinates
+
+        end_angle: desired angle of the end effector relative to horizontal, in radians
+        invert_base: set to True to "reach over backward" to hit the specified location
+        invert_elbow: set to True to reach the specified locaton with inverted elbow geometry
+        speed: maximum joint speed, servo units
+
+        returns: True if position is set, False if position is unachievable
+        """
+        angles = self.inverse_kinematics(x, y, z, end_angle, invert_base, invert_elbow)
+        print('Setting joint angles of {}, {}, {}, {} degrees'.format(angles.theta0*180/pi, angles.theta1*180/pi, angles.theta2*180/pi, angles.theta3*180/pi))
+        return self.set_joint_angles(angles, speed)
 
 
 BAUDRATE = 1000000
@@ -204,12 +363,54 @@ BAUDRATE = 1000000
 def test_servo():
     uart0 = UART(0, baudrate=BAUDRATE, tx=Pin(0), rx=Pin(1))
 
-    robot = Robot(uart0)
+    directionpin = Pin(2, Pin.OUT)
+    directionpin.value(0)
+    servo1 = AX12(uart0, 0x2, directionpin)
+
+    # servo1.write_position(512)
+    servo1.write_cw_limit(0)
+    servo1.write_torque_enable(False)
+
+    for i in range(1000):
+        response = servo1.read_position()
+        print('Position: ', response)
+        utime.sleep(0.5)
+
+    # i = 0
+    # while i < 10:
+    #     i += 1
+    #     print('\nAttempting to read temperature', i)
+    #     try:
+    #         print('Temperature (C):', servo1.read_temperature())
+    #         i = 100
+    #     except ValueError as e:
+    #         print('FAILED:', e)
+    # while 1:
+    #     pos1 = int(input("Position (0-1023): "))
+    #     print(servo1.write_position(pos1))
+
+
+def test_robot():
+    uart0 = UART(0, baudrate=BAUDRATE, tx=Pin(0), rx=Pin(1))
+
+
+    directionpin = Pin(2, Pin.OUT)
+    directionpin.value(0)
+
+    robot = Robot(AX12(uart0, 0x1, directionpin),
+                  AX12(uart0, 0x2, directionpin),
+                  AX12(uart0, 0x3, directionpin),
+                  AX12(uart0, 0x4, directionpin))
 
     while 1:
-        num = int(input("Joint number (1-4): "))
-        pos = int(input("Position (0-1023): "))
-        robot.set_joint(num, pos)
+        coords = input("Coordinates: ")
+        coords = [float(c) for c in coords.split()]
+        if len(coords) < 3:
+            print('invalid input')
+            continue
+        print(robot.set_position(*coords))
 
-test_servo()
+if __name__ == '__main__':
+    test_robot()
+    # test_servo()
 
--- pyworkspace/kinematics.py
+++ pyworkspace/kinematics.py
@@ -1,20 +1,14 @@
+from math import atan2, cos, pi, sin
 import numpy as np
+import sys
+from unittest.mock import MagicMock
 
-# Beam lengths
-l1 = 120
-l2 = 80
-l3 = 40
+sys.modules['machine'] = MagicMock()
+sys.modules['utime'] = MagicMock()
+from ax12 import Robot
 
-# Servo Angles
-t0 = 0
-t1 = np.pi / 2
-t2 = -2.84
-t3 = -0.8
-
-# 0
-# 0
-# 0
-
+# Wolfram alpha DH matrices
+# {{cos\(40)a\(41),0,sin\(40)a\(41),0},{sin\(40)a\(41),0,-cos\(40)a\(41),0},{0,1,0,0},{0,0,0,1}}{{cos\(40)b\(41),-sin\(40)b\(41),0,l*cos\(40)b\(41)},{sin\(40)b\(41),cos\(40)b\(41),0,l*sin\(40)b\(41)},{0,0,1,0},{0,0,0,1}}{{cos\(40)c\(41),-sin\(40)c\(41),0,m*cos\(40)c\(41)},{sin\(40)c\(41),cos\(40)c\(41),0,m*sin\(40)c\(41)},{0,0,1,0},{0,0,0,1}}{{cos\(40)d\(41),-sin\(40)d\(41),0,n*cos\(40)d\(41)},{sin\(40)d\(41),cos\(40)d\(41),0,n*sin\(40)d\(41)},{0,0,1,0},{0,0,0,1}}
 
 def get_dh_array(d, theta, r, alpha):
     return np.array(
@@ -24,16 +18,39 @@ def get_dh_array(d, theta, r, alpha):
          [0, 0, 0, 1]]
         )
 
-# Transformation matrices
-r01 = get_dh_array(0, t0, 0, np.pi / 2)
-r12 = get_dh_array(0, t1, l1, 0)
-r23 = get_dh_array(0, t2, l2, 0)
-r34 = get_dh_array(0, t3, l3, 0)
+# # Transformation matrices
+# r01 = get_dh_array(0, t0, 0, np.pi / 2)
+# r12 = get_dh_array(0, t1, l1, 0)
+# r23 = get_dh_array(0, t2, l2, 0)
+# r34 = get_dh_array(0, t3, l3, 0)
 
-transform = r01 @ r12 @ r23 @ r34
+# transform = r01 @ r12 @ r23 @ r34
 
-# print(np.round(transform[0:3, 3], 2))
-print(np.round(transform, 2))
+# # print(np.round(transform[0:3, 3], 2))
+# print(np.round(transform, 2))
+
+def get_forward(theta0, theta1, theta2, theta3):
+    r01 = get_dh_array(0, theta0, 0, np.pi / 2)
+    r12 = get_dh_array(0, theta1, Robot.L1, 0)
+    r23 = get_dh_array(0, theta2, Robot.L2, 0)
+    r34 = get_dh_array(0, theta3, Robot.L3, 0)
+
+    r04 = r01 @ r12 @ r23 @ r34
+    return r04[0:3, 3]
 
 
-# {{cos\(40)a\(41),0,sin\(40)a\(41),0},{sin\(40)a\(41),0,-cos\(40)a\(41),0},{0,1,0,0},{0,0,0,1}}{{cos\(40)b\(41),-sin\(40)b\(41),0,l*cos\(40)b\(41)},{sin\(40)b\(41),cos\(40)b\(41),0,l*sin\(40)b\(41)},{0,0,1,0},{0,0,0,1}}{{cos\(40)c\(41),-sin\(40)c\(41),0,m*cos\(40)c\(41)},{sin\(40)c\(41),cos\(40)c\(41),0,m*sin\(40)c\(41)},{0,0,1,0},{0,0,0,1}}{{cos\(40)d\(41),-sin\(40)d\(41),0,n*cos\(40)d\(41)},{sin\(40)d\(41),cos\(40)d\(41),0,n*sin\(40)d\(41)},{0,0,1,0},{0,0,0,1}}
+def check_inverse_math(x, y, z):
+    print(f"Target position ({x}, {y}, {z})")
+    theta0, theta1, theta2, theta3 = Robot.inverse_kinematics(x, y, z)
+    print(f"Joint angles ({theta0}, {theta1}, {theta2}, {theta3})")
+    real_pos = get_forward(theta0, theta1, theta2, theta3)
+    real_x = real_pos[0]
+    real_y = real_pos[1]
+    real_z = real_pos[2]
+    print("Actual position", real_pos)
+    dx = x - real_x
+    dy = y - real_y
+    dz = z - real_z
+    print(f"Error {round((dx**2 + dy**2 + dz**2)**0.5, 4)} ({dx}, {dy}, {dz})")
+
+check_inverse_math(144, 10, 137)
